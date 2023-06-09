00c9e3b801b73995f08acb841e8d8a4778d1d11e
alejah06 <124882889+alejah06@users.noreply.github.com>
2023-06-09 22:52:42 +0200

    Add files via upload

--- temperaturas.py
+++ temperaturas.py
@@ -5,48 +5,6 @@ Created on Fri Jun  9 20:11:55 2023
 @author: Alex
 """
 
-
-
-
-"""
-def main(hostname, list1, list2):
-
-    topic1 = "temperature/t1"
-    topic2 = "temperature/t2"
-
-    msg1 = subscribe.simple(topic1,
-                           hostname=hostname)
-    msg2 = subscribe.simple(topic2,
-                           hostname=hostname)
-    temp1 = msg1.payload.decode('utf8')
-    temp2 = msg2.payload.decode('utf8')
-    
-    list1 += float(temp1)
-    list2 += float(temp2)
-    
-    max1 = max(list1)
-    min1 = min(list1)
-    max2 = max(list2)
-    min2 = min(list2)
-    list1 = []
-    list2 = []
-    
-    print(f"El maximo de t1 es {max1}, y el minimo es {min1}\n")
-    print(f"El maximo de t2 es {max2}, y el minimo es {min2}\n")
-    
-    
-
-if __name__ == "__main__":
-    hostname = 'localhost'
-    list_int = []
-    list_float = []
-    if len(sys.argv)>1:
-        hostname = sys.argv[1]
-    while True:
-        main(hostname, list1, list2)
-"""
-
-import sys
 import time
 import multiprocessing
 import paho.mqtt.client as mqtt
